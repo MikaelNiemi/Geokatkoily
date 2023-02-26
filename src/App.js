3730c7e99b1a2a81cf6e94e7fdbc7c3eccd0255d
Mikael Niemi <n0nimi00@students.oamk.fi>
2023-02-26 09:52:39 +0200

    Toimintoja

--- src/App.js
+++ src/App.js
@@ -1,13 +1,117 @@
 import './App.css';
-import Vastaus from './Vastaus';
 import Kysymys from './Kysymys';
+import {useState} from 'react';
+import {Button} from '@mui/material';
+
 
 function App() {
-  return (
-    <div>
-      <Vastaus></Vastaus>
-    </div>
-  );
+    const [kysymykset, setKysymykset] = useState(0);
+    const [vastausN, setVastausN] = useState(0);
+    const [vastausW, setVastausW] = useState(0);
+    const [vastausJono, setVastausJono] = useState(0);
+    const [vastausJonoTeksti, setVastausJonoTeksti] = useState("");
+    const [vastaukset, setVastaukset] = useState(0);
+    const [i, setI] = useState(0);
+    
+    let a = i;
+    let oikeaVastaus = 0;
+
+    const FormPalkki = () => {
+        if (a === 2) {
+            return (
+                <div>
+                    <input 
+                    type="text" 
+                    value={vastausJonoTeksti} 
+                    onChange={e => setVastausJonoTeksti(e.target.value)}></input>
+                </div>
+            );
+        } else {
+            return (
+                <div>
+                    <div>
+                        <label>
+                            Lat:
+                            <select>
+                                <option selected value="North">N</option>
+                                <option value="South">S</option>
+                            </select>
+                            <input type="number" 
+                            value={vastausN} 
+                            onChange={e => setVastausN(e.target.value)}></input>
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            Lon:
+                            <select>
+                                <option selected value="East">E</option>
+                                <option value="West">W</option>
+                            </select>
+                            <input type="number" 
+                            value={vastausW} 
+                            onChange={e => setVastausW(e.target.value)}></input>
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            Tai naatit jonona:
+                            <input name="naattiJono" type="number"
+                            value={vastausJono} 
+                            onChange={e => setVastausJono(e.target.value)}></input>
+                        </label>
+                    </div>
+                </div>
+            );
+        }
+    }
+
+    const KysymyksetEteenpain = () => {
+            if (vastausJono || vastausJonoTeksti == oikeaVastaus) {
+                    
+                    a++;
+                    setI(a);
+                    console.log(a);
+                    setKysymykset(a);
+                    setVastaukset(a);
+            }
+        }
+
+   
+
+        switch(vastaukset) {
+            case 0:
+                oikeaVastaus = 60;
+            break;
+
+            case 1:
+                oikeaVastaus = 16;
+            break;
+
+            case 2:
+                oikeaVastaus = "Sianliha";
+            break;
+
+            case 3:
+                oikeaVastaus = 44;
+            break;
+
+            case 4:
+                oikeaVastaus = 51;
+            break;
+        }
+
+    return (
+        <div>
+            <Kysymys KysymyksetEteenpain={kysymykset}></Kysymys>
+
+
+            <form>
+                {FormPalkki()}
+                <Button onClick={() => KysymyksetEteenpain()}>Primary</Button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
--- src/Kysymys.js
+++ src/Kysymys.js
@@ -1,9 +1,38 @@
 import './App.css';
+import {useState} from 'react';
 
-function Kysymys() {
+function Kysymys({KysymyksetEteenpain}) {
+    let kysymys = "";
+
+    switch(KysymyksetEteenpain) {
+        case 0:
+            kysymys = "Koordinaatit ovat muotoa:"
+            + "\n"
+            + "60° XX.XXX 022° XX.XXX"
+            + "\n"
+            + "Matti oli elämässään tavannut 20 upeata perhettä. Hän päätti tehdä heistä laulun. Rosenlewin perhe oli todella säkenöivä ja heidän kanssaan Matilla oli ollut aina hauskaa. Wasastjernan perheessä oli äiti, isä ja kaksi vallatonta lasta. Lapsien kanssa sattui ja tapahtui. Matilla ei ollut omia lapsia, joten heidän vilkkautensa ei häntä haitannut. Päinvastoin, hänestä oli hauska seurata lasten kommelluksia. Frenckell-perhe oli vielä hieman vieras. He olivat vielä tutustumisvaiheessa. Perheen isä oli klovni ja Matti kävi hänen esityksiään katsomassa aina kun voi. Björnbergit hän on tuntenut jo lapsesta asti. Heidän perheen äiti ja isä olivat hänen lapsuudenkavereitaan. Perhe oli hänelle kaikkein läheisin. Serlachiukset olivat muuttaneet ulkomailta ja heillä oli aina mukavia kertomuksia Afrikan viidakosta. Serlachius oli hyvin harvinainen nimi ja Matilla oli vaikeuksia ääntää nimi oikein. Virkkusten isä oli nimensä mukainen virkkunen. Isä heräsi kukonlaulun aikaan joka aamu. Onneksi hän ei silti soitellut Matille siihen aikoihin. Matti tykkäsi nukkua pitkään. Kaikkien nimiä ei tässä kohtaa nyt mainita. Viimeisin perhe pitää vielä mainita, sillä Waldenin perheen tytär menee naimisiin Matin siskonpojan kanssa. Se on Matille tärkeä asia, sillä siskonpoika on hänen kummipoikansa. Hän sisällyttää senkin asian lauluunsa. Laulu julkaistaan helmikuussa 2030. ";
+        break;
+
+        case 1:
+            kysymys = "moi";
+        break;
+
+        case 2:
+            kysymys = "testi";
+        break;
+
+        case 3:
+            kysymys = "lisää";
+        break;
+
+        case 4:
+            kysymys = "nonii";
+        break;
+    }
+    
     return (
         <div>
-
+            <p>{kysymys}</p>
         </div>
     );
 }
